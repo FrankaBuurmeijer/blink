a8d2f6c25474efc65ab10b84e333d4bfa738763e
franka <franka.buurmeijer@gmail.com>
2021-01-20 23:00:06 +0100

    Scenario_Wereld_natuur_techniek_groep6_thema1_kringloop_leven_beschikbaar

---
+++ cypress/integration/proeflesflow/wereld_natuur_techniek_groep6_thema1_kringloop_leven_beschikbaar.spec.js
@@ -0,0 +1,16 @@
+describe('Wereld Natuur en Techniek Groep 6 Thema 1 Kringloop van het leven is beschikbaar', () => {
+    beforeEach(() => {
+        cy.visit('​https://studio.blink.nl')
+      })
+
+    it('Gebruiker met proeflesaacount logt in', () => {
+        cy.get('[type="email"]')
+        cy.type('franka.buurmeijer@gmail.com')
+        cy.get('[type="password"]')
+        cy.type('Blink123')
+        cy.click('.button_primary')
+    
+        expect(cy.title(Home - Studio)).to.equal(true)
+    })
+  })
+  
